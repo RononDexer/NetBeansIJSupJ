fd1073394277f4364353a31f110e1e90c57ffe56
RononDexer <ronon.dexer@gmail.com>
2014-04-21 23:05:54 +0200

    modif de Spectra et XYPlotSp pour calc et afficher images

--- plugins/lib/XYPlotSp.java
+++ plugins/lib/XYPlotSp.java
@@ -1,4 +1,6 @@
 package lib;
+import SupavisioJ.ImageGenerated.ImageGenerated;
+import SupavisioJ.Spectra.Spectra;
 import java.awt.Color;
 import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
@@ -18,6 +20,7 @@ import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
 import ij.*;
 import java.awt.Dimension;
+import java.util.ArrayList;
 import java.util.Vector;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.ParallelGroup;
@@ -31,10 +34,12 @@ import org.jfree.chart.ChartUtilities;
 public class XYPlotSp extends JFrame {
     
     private String titleGraph;
+    private Spectra spectraDrew;
     
-    public XYPlotSp(final String titleWindow,final String titleGraph2, double[] energiesX, double[] dataY) {
+    public XYPlotSp(Spectra spectraDrew, final String titleWindow,final String titleGraph2, double[] energiesX, double[] dataY) {
         super(titleWindow);
         this.titleGraph=titleGraph2;
+        this.spectraDrew=spectraDrew;
         //create the dataset
         final XYDataset dataset = createDataset(energiesX,dataY);
         //create the chart
@@ -250,7 +255,47 @@ public class XYPlotSp extends JFrame {
     
     private void jButtonGenImgActionPerformed(java.awt.event.ActionEvent evt) {                                         
         // TODO gen img for all checkbox selected
-        IJ.log("fonctionnalité non codée pour le moment");
+        //First which case is selected
+        // number of case = vectButtonsSupp.size()
+        // the whole is taken if JComp[0] is selected and 1,2 et 3 are corrects
+        try {
+            ImageGenerated[] tabImgGenFromSpectra;
+            ArrayList<String> nameOfImgGen = new ArrayList<String>();
+            ArrayList<float[]> minMaxSpectra = new ArrayList<float[]>();
+            for(int i=0; i<vectButtonsSupp.size();i++){
+                JComponent[] tabJCompToCheck = (JComponent[]) vectButtonsSupp.get(i);
+                JCheckBox checkBoxCurrent = (JCheckBox) tabJCompToCheck[0];
+                if(checkBoxCurrent.isSelected()){
+                    JTextField name= (JTextField) tabJCompToCheck[1];
+                    JTextField min= (JTextField) tabJCompToCheck[2];
+                    JTextField max=(JTextField) tabJCompToCheck[3];
+                    if(!(name.getText().equals("name") || min.getText().equals("min") || max.getText().equals("max"))){
+                        float start=-1;
+                        float end=-1;
+                        try {
+                            start = Float.valueOf(min.getText());
+                            end = Float.valueOf(max.getText());
+                        }
+                        catch(NumberFormatException e){
+                            IJ.log("Mettez des chiffres dans les champs min et max");
+                        }
+                        if (spectraDrew.energyExist(start) && spectraDrew.energyExist(end)){
+                            nameOfImgGen.add(name.getText());
+                            float[] tabMinMax = new float[2];
+                            tabMinMax[0]=start;
+                            tabMinMax[1]=end;
+                            minMaxSpectra.add(tabMinMax);
+                        }
+                    }
+                }
+            }
+            float[][] minMaxSpectraArray = minMaxSpectra.toArray(new float[minMaxSpectra.size()][2]);
+            tabImgGenFromSpectra = spectraDrew.generatePicture(minMaxSpectraArray);
+            for (int i=0;i<tabImgGenFromSpectra.length;i++){
+                tabImgGenFromSpectra[i].show(nameOfImgGen.get(i));
+            }
+        }
+        catch(NullPointerException e){}
     }
     
     private void jButtonLogLinActionPerformed(java.awt.event.ActionEvent evt) {                                         
