193c2cce0c980e71d18e14b15f1d6a923c296f7a
RononDexer <ronon.dexer@gmail.com>
2014-05-25 17:24:36 +0200

    maj *pas important* rearrangement d'import

--- plugins/Supavisio_J.java
+++ plugins/Supavisio_J.java
@@ -1,11 +1,5 @@
 import SupavisioJ.MainFrame.MainFrame;
-import ij.IJ;
 import ij.plugin.*;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import javax.swing.SwingUtilities;
 
 /**
